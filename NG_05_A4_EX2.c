a1605a2d098909ef6020790cee2d1a594fea3baa
NathanXGardner <42708341+NathanXGardner@users.noreply.github.com>
2019-02-06 00:13:15 -0600

    Add files via upload

---
+++ NG_05_A4_EX2.c
@@ -0,0 +1,76 @@
+/* Nathan Gardner
+   CS 1325.504
+   GradeBookNumber: 05
+   This program quizzes the user over the USA's state capitals.
+   The program asks the user to enter a string using the fgets function.
+   This function reads data until a null terminator is found, or the max
+   character length is met minus one (to account for the null
+	terminator). The user's array of characters is then compared to the
+	specific pointer of the capital names' array using the string comparison
+	function strcmp, and then a value of 0 (false) is returned if they are
+	equal. This function takes the number of questions the
+	user answered, the number of questions the user correctly answered, and
+	the percentage of questions the user correctly answered, and prints these
+	data items on the screen.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "statesAndCapitals.h"
+
+int main()
+{
+	srand(time(NULL));
+
+   int sentinel = 1;
+	char garbage;
+	unsigned int question;
+	unsigned int total_questions = 0, correct_answers = 0;
+	char userAnswer[MAX_CAPITAL_LEN + 1] = {""};
+
+	puts("Do you know your state capitals?");
+
+	while (1 == sentinel)
+	{
+		question = rand() % 50;
+		printf("\nWhat is the capital of %s?\n", states[question]);
+
+		fgets(userAnswer, MAX_CAPITAL_LEN + 1, stdin);
+
+		if (0 == strcmp(userAnswer, capitals[question]))
+		{
+			printf("You are correct. The capital of %s is %s\n", states[question], capitals[question]);
+			correct_answers++;
+		}
+
+		else
+		{
+			printf("You are incorrect. The capital of %s is %s\n", states[question], capitals[question]);
+		}
+
+		total_questions++;
+
+		puts("Enter 1 for another question, 0 to end.");
+		scanf("%d", &sentinel);
+
+		while (sentinel != 0 && sentinel != 1)
+		{
+			printf("You entered %d.  This is an incorrect value.\n", sentinel);
+			puts("Make sure to input a zero to quit or a 1 to continue\n");
+
+			puts("Enter 1 for another question, 0 to end.");
+			scanf("%d", &sentinel);
+		}
+
+		scanf("%c", &garbage);
+	}
+
+	double percent = ((double)correct_answers / total_questions) * 100;
+
+	printf("\nYou answered %d correctly out of %d for %f percent correct.",
+			  correct_answers, total_questions, percent);
+
+	return 0;
+}
---
+++ statesAndCapitals.h
@@ -0,0 +1,26 @@
+#define NUM_STATES 50
+#define MAX_CAPITAL_LEN 20
+
+
+
+const char *states[NUM_STATES] = {"Alabama", "Alaska", "Arizona", "Arkansas", "California",
+                                  "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+                                  "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+                                  "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+                                  "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+                                  "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+                                  "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+                                  "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+                                  "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+                                  "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" };
+
+const char *capitals[NUM_STATES] = {"Montgomery\n", "Juneau\n", "Phoenix\n", "Little Rock\n", "Sacramento\n",
+                                    "Denver\n", "Hartford\n", "Dover\n", "Tallahassee\n", "Atlanta\n",
+                                    "Honolulu\n", "Boise\n", "Springfield\n", "Indianapolis\n", "Des Moines\n",
+                                    "Topeka\n", "Frankfort\n", "Baton Rouge\n", "Augusta\n", "Annapolis\n",
+                                    "Boston\n", "Lansing\n", "St. Paul\n", "Jackson\n", "Jefferson City\n",
+                                    "Helena\n", "Lincoln\n", "Carson City\n", "Concord\n", "Trenton\n",
+                                    "Santa Fe\n", "Albany\n", "Raleigh\n", "Bismarck\n", "Columbus\n",
+                                    "Oklahoma City\n", "Salem\n", "Harrisburg\n", "Providence\n", "Columbia\n",
+                                    "Pierre\n", "Nashville\n", "Austin\n", "Salt Lake City\n", "Montpelier\n",
+                                    "Richmond\n", "Olympia\n", "Charleston\n", "Madison\n", "Cheyenne\n" };
